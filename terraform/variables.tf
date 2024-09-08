181772e47638c6f28917a1571146a44d221681d2
wisdom <wisdomolaniyan@yahoo.com>
2024-09-08 17:37:52 +0100

    ci-cd pipeline added

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -50,8 +50,8 @@ variable "subnet_cidrs" {
   description = "List of CIDR blocks for the subnets"
   type        = list(string)
   default = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24"
+    "10.0.1.0/24", # Should be in AZ 1
+    "10.0.2.0/24", # Should be in AZ 2
+    "10.0.3.0/24"  # Should be in AZ 3
   ]
 }
