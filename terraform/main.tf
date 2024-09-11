88009210faf87b79f0df89c9421b2c34acd4b76f
wisdom <wisdomolaniyan@yahoo.com>
2024-09-11 13:47:43 +0100

    ci-cd pipeline update vpc cleared iam role updated for terraform to check first

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.57.0"
+      version = "~>5.0"
     }
   }
-}
+  required_version = ">= 1.3.0"
 
+}
 provider "aws" {
   region = var.region
 }
